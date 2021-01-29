a62a25efd714a8a22bb67212978ec7b90f65b098
Rogerio bispo <rogerio_pd@yahoo.com.br>
2021-01-29 20:13:28 -0300

    unplementations of rabbitMQ provider

---
+++ Users-api/src/shared/container/providers/messageBrokerProvider/implementations/rabbitMQ.ts
@@ -0,0 +1,41 @@
+import Amqp from 'amqplib';
+import { RabbitMQConfig } from '../../../../config';
+import IMessageBroker from '../models/IMessageBrocker';
+
+class RabbitMQConnection implements IMessageBroker {
+  private connection;
+
+  constructor() {
+    this.connection = Amqp.connect({
+      hostname: RabbitMQConfig.host,
+      port: RabbitMQConfig.port,
+      vhost: RabbitMQConfig.vhost,
+      username: RabbitMQConfig.user,
+      password: RabbitMQConfig.pwd,
+    });
+  }
+
+  async consume(queue: string, callBack: (msg: any) => void): Promise<void> {
+    const channel = await this.getChannel();
+    await channel.consume(queue, callBack);
+  }
+
+  async publish(
+    exchange: string,
+    message: string,
+    routingKey = '*'
+  ): Promise<boolean> {
+    const channel = await this.getChannel();
+    const content = Buffer.from(message);
+    return channel.publish(exchange, routingKey, content);
+  }
+
+  private getChannel() {
+    return this.connection
+      .then((conn) => conn.createChannel())
+      .then((channel) => channel);
+  }
+}
+const rabbitMQConnection = new RabbitMQConnection();
+
+export default rabbitMQConnection;
---
+++ Users-api/src/shared/container/providers/messageBrokerProvider/models/IMessageBrocker.ts
@@ -0,0 +1,10 @@
+interface IMessageBroker {
+  publish(
+    exchange: string,
+    message: string,
+    routingKey?: string
+  ): Promise<boolean>;
+  consume(queue: string, callBack: (msg: any) => void): void;
+}
+
+export default IMessageBroker;
