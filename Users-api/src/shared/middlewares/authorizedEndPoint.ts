ca4ac5894aab5324b92f0ef7d40592f86b122d74
Rogerio bispo <rogerio_pd@yahoo.com.br>
2021-02-01 11:59:23 -0300

    endpoint login and middleware to protectec endpoints

--- Users-api/src/shared/middlewares/authorizedEndPoint.ts
+++ Users-api/src/shared/middlewares/authorizedEndPoint.ts
@@ -4,19 +4,15 @@ import { promisify } from 'util';
 import { JwtConfig } from '../config';
 import AppError from '../errors/AppErrors';
 
-const { secret } = JwtConfig;
-
 export default async function authorizedEndPoint(
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> {
   try {
-    const [, token] = req.headers.authorization.split(' ');
-
+    const token = req.headers?.authorization?.split(' ')[1];
     if (!token) res.status(401).json({ error: 'Unauthorized' });
-
-    await promisify(JWT.verify)(token);
+    if (token) JWT.verify(token, JwtConfig.secret);
     next();
   } catch (error) {
     throw new AppError('Unauthorized', 401);
