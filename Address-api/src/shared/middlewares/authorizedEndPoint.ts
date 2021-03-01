d93765dd382e6b746bde8b7374f5baa99e9ac3a5
Rogerio bispo <rogerio_pd@yahoo.com.br>
2021-03-01 01:54:50 -0300

    authorized end point

--- Address-api/src/shared/middlewares/authorizedEndPoint.ts
+++ Address-api/src/shared/middlewares/authorizedEndPoint.ts
@@ -12,13 +12,17 @@ export default async function authorizedEndPoint(
   next: NextFunction
 ): Promise<void> {
   try {
-    const token = req.headers?.authorization?.split(' ')[1];
+    const token = req.headers?.authorization;
+
     if (!token) throw new AppError('Unauthorized', 401);
     if (token) {
-      await api.get(`/authorized/${token}`);
+      await api.get('/authorized', {
+        headers: { authorization: token },
+      });
     }
     next();
   } catch (error) {
+    console.log(error);
     throw new AppError('Unauthorized', 401);
   }
 }
